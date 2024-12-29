6526ac395188a3702c5cb3e0eced0e2ef3551763
Emre <emreetumer0@gmail.com>
2024-12-29 16:57:04 +0300

    11.Commit
    User kullanıcısı için sayfalar oluşturuldu.

---
+++ 11.Commit/navigators/UserTabNavigator.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import HomeScreen from '../screens/HomeScreen';
+import UserSettingsScreen from '../screens/UserSettingsScreen';
+
+const Tab = createBottomTabNavigator();
+
+const UserTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Settings') {
+            iconName = 'settings';
+          }
+
+          return <MaterialIcons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#1ABC9C',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={UserSettingsScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default UserTabNavigator;
---
+++ 11.Commit/screens/LoginScreen.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { auth, db } from '../firebaseConfig'; // Firebase modüllerini import et
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      Alert.alert('Hata', 'Lütfen tüm alanları doldurun');
+      return;
+    }
+
+    try {
+      // Firebase Authentication ile giriş
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      // Kullanıcının rolünü almak için Firestore sorgusu
+      const userDocRef = doc(db, 'users', user.uid);
+      const userDoc = await getDoc(userDocRef);
+
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+
+        // Kullanıcı rolüne göre yönlendirme
+        if (userData.role === 'admin') {
+          navigation.navigate('Admin'); // Admin navigasyonu
+        } else if (userData.role === 'user') {
+          navigation.navigate('User'); // User navigasyonu
+        } else {
+          Alert.alert('Hata', 'Rol bilgisi tanımlı değil.');
+        }
+      } else {
+        Alert.alert('Hata', 'Kullanıcı bilgileri bulunamadı.');
+      }
+    } catch (error) {
+      Alert.alert('Giriş Hatası', error.message);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.iconContainer}>
+        <MaterialIcons name="science" size={120} color="#1ABC9C" />
+      </View>
+      <Text style={styles.title}>LabNet</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email Adresi"
+        placeholderTextColor="#aaa"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Şifre"
+        secureTextEntry={true}
+        placeholderTextColor="#aaa"
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Giriş Yap</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
+        <Text style={styles.link}>Kayıt olmak ister misiniz?</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F9FBFC',
+    padding: 20,
+  },
+  iconContainer: {
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#34495E',
+    marginBottom: 30,
+  },
+  input: {
+    width: '90%',
+    height: 40,
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    fontSize: 14,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  button: {
+    backgroundColor: '#1ABC9C',
+    width: '50%',
+    height: 45,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  link: {
+    color: '#3498DB',
+    fontSize: 16,
+    marginTop: 15,
+    textDecorationLine: 'underline',
+  },
+});
+
+export default LoginScreen;
